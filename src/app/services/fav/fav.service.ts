44bd61dba0fb12d2383fcb81485c4dad185fd229
Mohamed Tarek <MohamedTarek153@gmail.com>
2021-03-20 00:09:53 +0200

    ass 3

--- src/app/services/fav/fav.service.ts
+++ src/app/services/fav/fav.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Dish } from '../../shared/dish';
 import { DishService } from '../dish/dish.service';
 import { Observable, throwError } from 'rxjs';
+import { Storage } from '@ionic/storage';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -11,13 +12,19 @@ export class FavService {
 
   favs: Array<any>;
 
-  constructor(private dishService: DishService) { 
-    this.favs = [];
+  constructor(private dishService: DishService, private storage: Storage) { 
+    this.storage.get('favs').then(async favs=> {
+      if(favs){
+        this.favs = favs;
+      }else
+      this.favs =[];
+    });
   }
 
   addFav(id:string): boolean {
     if(!this.isFav(id)){
       this.favs.push(id);
+      this.storage.set('favs',this.favs);
     }
     return true;
   }
@@ -26,6 +33,7 @@ export class FavService {
     let index = this.favs.indexOf(id);
     if(index >= 0){
       this.favs.splice(index, 1);
+      this.storage.set('favs',this.favs);
       return this.getFavs();
     }else {
       return throwError('a7a btms7 ah !');
