e230cd6a4ea6ae022d9eb2437eff729c7f318ef7
Mohamed Tarek <MohamedTarek153@gmail.com>
2021-03-30 12:36:16 +0200

    network

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Platform, ModalController, LoadingController } from '@ionic/angular';
 import { ReservationPage } from './reservation/reservation.page';
 import { LoginPage } from './login/login.page';
+import { Network } from '@ionic-native/network/ngx';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,9 @@ import { LoginPage } from './login/login.page';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
+  loading: any = null;
+
   public appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Menu', url: '/menu', icon: 'list' },
@@ -16,7 +20,30 @@ export class AppComponent {
     { title: 'About US', url: '/about', icon: 'information-circle' },
     { title: 'Contact US', url: '/contact', icon: 'call' }
   ];
-  constructor(private modalCtrl: ModalController, @Inject('BaseURL') public BaseURL:string) {}
+  constructor(private platform: Platform, private network: Network, private modalCtrl: ModalController, private loadingCtrl: LoadingController, @Inject('BaseURL') public BaseURL:string) {
+    this.platform.ready().then(() => {
+      this.network.onDisconnect().subscribe(() => {
+        if (!this.loading) {
+          this.loading = this.loading.create({
+            message: 'Adding . . . . '
+          });
+          this.loading.present();
+        }
+      });
+
+      this.network.onConnect().subscribe(() => {
+        setTimeout(() => {
+          if (this.network.type === 'wifi') {
+            console.log('we got a wifi connection, woohoo!');
+          }
+        }, 3000);
+        if (this.loading) {
+          this.loading.dismiss();
+          this.loading = null;
+        }
+      });
+    });
+  }
 
   async presentReserve() {
     const modal = await this.modalCtrl.create({
